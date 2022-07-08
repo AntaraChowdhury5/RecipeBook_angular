bb93679129881272e0c2c77c4177df9228a5f4b3
AntaraChowdhury5 <antara.chowdhury@quovantis.com>
2022-07-09 02:19:29 +0530

    Added: databinding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,13 +7,18 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected=new EventEmitter<Recipe>();
   recipes:Recipe[]=[
-    new Recipe('Cake','baked','https://imgcdn.floweraura.com/Vanilla-Fruit-Cake-9999520cb-291117-AAA.jpg')
+    new Recipe('Cake','baked','https://imgcdn.floweraura.com/Vanilla-Fruit-Cake-9999520cb-291117-AAA.jpg'),
+    new Recipe('Cockie','baked','https://data.whicdn.com/images/45919935/original.png')
   ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onRecipeSelected(recipe:Recipe){
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
